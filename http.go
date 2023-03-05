ba54358138c40632466373ead6ce81198234784a
AvicennaJr <justfuad@protonmail.com>
2023-03-05 20:43:52 +0300

    add request helper function

---
+++ http.go
@@ -0,0 +1,36 @@
+package sarufi
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// A helper function to make requests easier
+func (app *Application) makeRequest(method, url string, data io.Reader) (int, []byte, error) {
+	req, err := http.NewRequest(method, url, data)
+
+	if err != nil {
+		return 0, nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	bearer := fmt.Sprintf("Bearer %s", app.Token)
+	req.Header.Set("Authorization", bearer)
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return 0, nil, err
+	}
+
+	return resp.StatusCode, body, nil
+}
