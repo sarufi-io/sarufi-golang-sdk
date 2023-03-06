5043fd7b5c739fb01dc39029d479de879be18456
AvicennaJr <justfuad@protonmail.com>
2023-03-07 02:41:48 +0300

    separate application level logic

--- sarufi.go
+++ sarufi.go
@@ -10,10 +10,10 @@ import (
 // It accepts the bot id (type int) as a parameter.
 // Returns a pointer of type Bot and an error.
 func (app *Application) GetBot(id int) (*Bot, error) {
-	if !checkToken(app.Token) {
+	if !checkToken(token) {
 		return nil, fmt.Errorf("Error: No token available")
 	}
-	url := fmt.Sprintf("%schatbot/%d", app.BaseURL, id)
+	url := fmt.Sprintf("%schatbot/%d", baseURL, id)
 
 	statusCode, body, err := makeRequest("GET", url, nil)
 	if err != nil {
@@ -26,21 +26,27 @@ func (app *Application) GetBot(id int) (*Bot, error) {
 		if err := json.Unmarshal(body, &bot); err != nil {
 			return nil, err
 		}
-		// chatID := uuid.New()
-		// bot.chatID = chatID.String()
 		return bot, nil
-	case 404:
-		var notFound NotFoundError
-		if err := json.Unmarshal(body, &notFound); err != nil {
-			return nil, err
-		}
-		return nil, fmt.Errorf("Error %s", notFound.Error())
 	case 401:
 		var unauthorized Unauthorized
 		if err := json.Unmarshal(body, &unauthorized); err != nil {
 			return nil, err
 		}
 		return nil, fmt.Errorf("Error %s", unauthorized.Error())
+	case 404:
+		var notFound NotFoundError
+		if err := json.Unmarshal(body, &notFound); err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("Error %s", notFound.Error())
+	case 422:
+		var unprocessableEntity *UnprocessableEntity
+		if err := json.Unmarshal(body, &unprocessableEntity); err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("Error %s", unprocessableEntity.Error())
+	case 500:
+		return nil, fmt.Errorf("Error status code 500: Internal Server Error")
 	default:
 		return nil, fmt.Errorf(string(body))
 	}
@@ -48,11 +54,11 @@ func (app *Application) GetBot(id int) (*Bot, error) {
 
 // GetBots() method returns a list of type Bot and an error
 func (app *Application) GetBots() ([]Bot, error) {
-	if !checkToken(app.Token) {
+	if !checkToken(token) {
 		return nil, fmt.Errorf("Error: No token available")
 	}
 
-	url := app.BaseURL + "chatbots"
+	url := baseURL + "chatbots"
 
 	statusCode, body, err := makeRequest("GET", url, nil)
 	if err != nil {
@@ -65,14 +71,26 @@ func (app *Application) GetBots() ([]Bot, error) {
 			return nil, err
 		}
 		return bots, nil
-
 	case 401:
 		var unauthorized Unauthorized
 		if err := json.Unmarshal(body, &unauthorized); err != nil {
 			return nil, err
 		}
 		return nil, fmt.Errorf("Error %s", unauthorized.Error())
-
+	case 404:
+		var notFound NotFoundError
+		if err := json.Unmarshal(body, &notFound); err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("Error %s", notFound.Error())
+	case 422:
+		var unprocessableEntity *UnprocessableEntity
+		if err := json.Unmarshal(body, &unprocessableEntity); err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("Error %s", unprocessableEntity.Error())
+	case 500:
+		return nil, fmt.Errorf("Error status code 500: Internal Server Error")
 	default:
 		return nil, fmt.Errorf(string(body))
 	}
@@ -86,11 +104,11 @@ func (app *Application) GetBots() ([]Bot, error) {
 // Visible (type bool) - allow it to be publicly available
 // It returns a pointer to a new Bot and an error
 func (app *Application) CreateBot(name, description, industry string, visible bool) (*Bot, error) {
-	if !checkToken(app.Token) {
+	if !checkToken(token) {
 		return nil, fmt.Errorf("Error: No token available")
 	}
 
-	url := app.BaseURL + "chatbot"
+	url := baseURL + "chatbot"
 
 	params := map[string]interface{}{
 		"name":                 name,
@@ -113,14 +131,26 @@ func (app *Application) CreateBot(name, description, industry string, visible bo
 			return nil, err
 		}
 		return bot, nil
-
 	case 401:
 		var unauthorized Unauthorized
 		if err := json.Unmarshal(body, &unauthorized); err != nil {
 			return nil, err
 		}
 		return nil, fmt.Errorf("Error %s", unauthorized.Error())
-
+	case 404:
+		var notFound NotFoundError
+		if err := json.Unmarshal(body, &notFound); err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("Error %s", notFound.Error())
+	case 422:
+		var unprocessableEntity *UnprocessableEntity
+		if err := json.Unmarshal(body, &unprocessableEntity); err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("Error %s", unprocessableEntity.Error())
+	case 500:
+		return nil, fmt.Errorf("Error status code 500: Internal Server Error")
 	default:
 		return nil, fmt.Errorf(string(body))
 	}
@@ -129,7 +159,7 @@ func (app *Application) CreateBot(name, description, industry string, visible bo
 // UpdateBot() method to update the bot. It accepts a parameter of
 // type *Bot and will return  an error if any
 func (app *Application) UpdateBot(bot *Bot) error {
-	if !checkToken(app.Token) {
+	if !checkToken(token) {
 		return fmt.Errorf("Error: No token available")
 	}
 
@@ -138,7 +168,7 @@ func (app *Application) UpdateBot(bot *Bot) error {
 	if err != nil {
 		return err
 	}
-	url := fmt.Sprintf("%schatbot/%d", app.BaseURL, bot.Id)
+	url := fmt.Sprintf("%schatbot/%d", baseURL, bot.Id)
 	statusCode, body, err := makeRequest("PUT", url, bytes.NewBuffer(jsonParams))
 	if err != nil {
 		return err
@@ -150,14 +180,26 @@ func (app *Application) UpdateBot(bot *Bot) error {
 			return err
 		}
 		return nil
-
 	case 401:
 		var unauthorized Unauthorized
 		if err := json.Unmarshal(body, &unauthorized); err != nil {
 			return err
 		}
 		return fmt.Errorf("Error %s", unauthorized.Error())
-
+	case 404:
+		var notFound NotFoundError
+		if err := json.Unmarshal(body, &notFound); err != nil {
+			return err
+		}
+		return fmt.Errorf("Error %s", notFound.Error())
+	case 422:
+		var unprocessableEntity *UnprocessableEntity
+		if err := json.Unmarshal(body, &unprocessableEntity); err != nil {
+			return err
+		}
+		return fmt.Errorf("Error %s", unprocessableEntity.Error())
+	case 500:
+		return fmt.Errorf("Error status code 500: Internal Server Error")
 	default:
 		return fmt.Errorf(string(body))
 	}
@@ -167,11 +209,11 @@ func (app *Application) UpdateBot(bot *Bot) error {
 // DeleteBot() will delete the bot of with provided ID.
 // It will return an error if deletion was unsuccessful
 func (app *Application) DeleteBot(id int) error {
-	if !checkToken(app.Token) {
+	if !checkToken(token) {
 		return fmt.Errorf("Error: No token available")
 	}
 
-	url := fmt.Sprintf("%schatbot/%d", app.BaseURL, id)
+	url := fmt.Sprintf("%schatbot/%d", baseURL, id)
 	statusCode, body, err := makeRequest("DELETE", url, nil)
 	if err != nil {
 		return err
@@ -180,21 +222,26 @@ func (app *Application) DeleteBot(id int) error {
 	switch statusCode {
 	case 200:
 		return nil
-
 	case 401:
 		var unauthorized Unauthorized
 		if err := json.Unmarshal(body, &unauthorized); err != nil {
 			return err
 		}
 		return fmt.Errorf("Error %s", unauthorized.Error())
-
 	case 404:
 		var notFound NotFoundError
 		if err := json.Unmarshal(body, &notFound); err != nil {
 			return err
 		}
 		return fmt.Errorf("Error %s", notFound.Error())
-
+	case 422:
+		var unprocessableEntity *UnprocessableEntity
+		if err := json.Unmarshal(body, &unprocessableEntity); err != nil {
+			return err
+		}
+		return fmt.Errorf("Error %s", unprocessableEntity.Error())
+	case 500:
+		return fmt.Errorf("Error status code 500: Internal Server Error")
 	default:
 		return fmt.Errorf(string(body))
 	}
