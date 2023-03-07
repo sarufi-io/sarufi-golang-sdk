ca5e2f04a2665222b4ae3c82dcaa932530c0ca76
AvicennaJr <justfuad@protonmail.com>
2023-03-07 03:42:33 +0300

    add ability to make choice flows

--- types.go
+++ types.go
@@ -8,22 +8,22 @@ type Application struct {
 // Type Bot. All the fields are matched to the API JSON response.
 // You can read more here https://neurotech-africa.stoplight.io/docs/sarufi/a3135fbb09470-create-new-chatbot
 type Bot struct {
-	Id                    int                 `json:"id"`
-	Name                  string              `json:"name"`
-	Industry              string              `json:"industry"`
-	Description           string              `json:"description"`
-	UserID                int                 `json:"user_id"`
-	Username              string              `json:"user_name"`
-	VisibleOnCommunity    bool                `json:"visible_on_community"`
-	Intents               map[string][]string `json:"intents"`
-	Flows                 Flows               `json:"flows"`
-	ModelName             string              `json:"model_name"`
-	WebhookURL            string              `json:"webhook_url"`
-	WebhookTriggerIntents []string            `json:"webhook_trigger_intents"`
-	EvaluationMetrics     interface{}         `json:"evaluation_metrics"`
-	CreatedAt             string              `json:"created_at"`
-	UpdatedAt             string              `json:"updated_at"`
-	ChatID                string              `json:"chat_id"`
+	Id                    int                    `json:"id"`
+	Name                  string                 `json:"name"`
+	Industry              string                 `json:"industry"`
+	Description           string                 `json:"description"`
+	UserID                int                    `json:"user_id"`
+	Username              string                 `json:"user_name"`
+	VisibleOnCommunity    bool                   `json:"visible_on_community"`
+	Intents               map[string][]string    `json:"intents"`
+	Flows                 map[string]interface{} `json:"flows"`
+	ModelName             string                 `json:"model_name"`
+	WebhookURL            string                 `json:"webhook_url"`
+	WebhookTriggerIntents []string               `json:"webhook_trigger_intents"`
+	EvaluationMetrics     interface{}            `json:"evaluation_metrics"`
+	CreatedAt             string                 `json:"created_at"`
+	UpdatedAt             string                 `json:"updated_at"`
+	ChatID                string                 `json:"chat_id"`
 	Conversation          Conversation
 	Prediction            Prediction
 	ChatUsers             []ChatUser
@@ -79,4 +79,4 @@ type Flow struct {
 }
 
 // Type Flows to map a string to the type Flow.
-type Flows map[string]Flow
+type Flows map[string]interface{}
