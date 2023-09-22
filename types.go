2e12d3cc4dd11f1126a999534ca4e88763b885d2
AvicennaJr <justfuad@protonmail.com>
2023-09-22 13:36:52 +0300

    add response types for knowledge based bots

--- types.go
+++ types.go
@@ -3,28 +3,29 @@ package sarufi
 // Type Bot. All the fields are matched to the API JSON response.
 // You can read more here https://neurotech-africa.stoplight.io/docs/sarufi/a3135fbb09470-create-new-chatbot
 type Bot struct {
-	Id                    int                    `json:"id"`
-	Name                  string                 `json:"name"`
-	Industry              string                 `json:"industry"`
-	Description           string                 `json:"description"`
-	UserID                int                    `json:"user_id"`
-	VisibleOnCommunity    bool                   `json:"visible_on_community"`
-	Intents               map[string][]string    `json:"intents"`
-	Flows                 map[string]interface{} `json:"flows"`
-	ModelName             string                 `json:"model_name"`
-	WebhookURL            string                 `json:"webhook_url"`
-	WebhookTriggerIntents []string               `json:"webhook_trigger_intents"`
-	EvaluationMetrics     interface{}            `json:"evaluation_metrics"`
-	ChatID                string                 `json:"chat_id"`
-	Conversation          Conversation
-	Prediction            Prediction
-	ChatUsers             []ChatUser
-	ConversationHistory   []ConversationHistory `json:"conversation_history"`
+	Id                        int                    `json:"id"`
+	Name                      string                 `json:"name"`
+	Industry                  string                 `json:"industry"`
+	Description               string                 `json:"description"`
+	UserID                    int                    `json:"user_id"`
+	VisibleOnCommunity        bool                   `json:"visible_on_community"`
+	Intents                   map[string][]string    `json:"intents"`
+	Flows                     map[string]interface{} `json:"flows"`
+	ModelName                 string                 `json:"model_name"`
+	WebhookURL                string                 `json:"webhook_url"`
+	WebhookTriggerIntents     []string               `json:"webhook_trigger_intents"`
+	EvaluationMetrics         interface{}            `json:"evaluation_metrics"`
+	ChatID                    string                 `json:"chat_id"`
+	Conversation              Conversation
+	ConversationWithKnowledge ConversationWithKnowledge
+	Prediction                Prediction
+	ChatUsers                 []ChatUser
+	ConversationHistory       []ConversationHistory `json:"conversation_history"`
 }
 
 // This type will be used in the conversation history
 type Response struct {
-	SendMessage []string `json:"send_message"`
+	Message []string `json:"send_message"`
 }
 
 // This type will hold information of the converstion
@@ -54,13 +55,26 @@ type Prediction struct {
 }
 
 // Conversation will hold all conversation
-// related data
+// related data on none knowledge base bot
 type Conversation struct {
-	Message      []string          `json:"message"`
-	Memory       map[string]string `json:"memory"`
-	CurrentState string            `json:"current_state"`
-	NextState    string            `json:"next_state"`
+	Message   []string `json:"message"`
+	Memory    Memory   `json:"memory"`
+	NextState string   `json:"next_state"`
 }
+
+// ConversationWithKnowledge will hold all conversation
+// related data on knowledge base bot
+type ConversationWithKnowledge struct {
+	Message   []Actions `json:"actions"`
+	Memory    Memory    `json:"memory"`
+	NextState string    `json:"next_state"`
+}
+
+type Actions struct {
+	ResponseMessage []any `json:"send_message"`
+}
+
+type Memory interface{}
 
 // Application will hold all application methods.
 // It also has details about the user
